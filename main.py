8e8fdecbb37d6a1a426b7b2f6c987a7a5cdf6422
Robert Siska <siska.robert@gmail.com>
2013-02-11 01:06:43 +0100

    demonstration

---
+++ main.py
@@ -0,0 +1,41 @@
+from galaxy import Galaxy
+from grid import GalaxyMap
+from pathfinder import find_path
+
+# startStar = [0, 0] # Sol
+# endStar = [-3, 0] # Tiafa
+# hyper-jump range = 8ly
+# --------------
+# - find the (shortest?) path
+# - show it on the map
+jumpRange = 8
+
+c = Galaxy()
+c.sanityTest()
+
+map = GalaxyMap((800, 800))
+
+sector1 = c.getSector(5912, 5412)
+sector2 = c.getSector(5909, 5412)
+
+sol = sector1.stars[0]
+tiafa = sector2.stars[0]
+
+path = find_path(c, sol, tiafa, jumpRange)
+
+prevStep = None
+if path is not None:
+    for step in path:
+        print step.name,
+        if (prevStep != None):
+            print ":", "(%.3f ly)" % prevStep.distance(step)
+        else:
+            print
+        prevStep = step
+
+map.path = path
+
+map.grid = True
+map.labels = True
+
+map.save(c, (0, 0), 6, 6)
